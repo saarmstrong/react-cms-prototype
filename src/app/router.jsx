751c96def689c72d968a819dc00d5681987e1c82
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2014-12-02 15:13:13 +0200

    move routes into separate file

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,69 +1,12 @@
 var React = require('react');
 var Router = require('react-router');
-var DocumentTitle = require('react-document-title');
-var App = require('./components/App.jsx');
-var People = require('./components/People.jsx');
-var Person = require('./components/Person.jsx');
-var Login = require('./components/Login.jsx');
-var Dashboard = require('./components/Dashboard.jsx');
-var RouteHandler = Router.RouteHandler;
-var Route = Router.Route;
-var DefaultRoute = Router.DefaultRoute;
-var NotFoundRoute = Router.NotFoundRoute;
-
-var NotFound = React.createClass({
-  render: function () {
-    return <p>Nope!</p>;
-  }
-});
-
-var Main = React.createClass({
-  render: function () {
-    return <RouteHandler {...this.props} />;
-  }
-});
-
-var PersonFoo = React.createClass({
-  render: function () {
-    return (
-      <DocumentTitle title="Person Foo">
-        <p>This is foo...</p>
-      </DocumentTitle>
-    );
-  }
-});
-
-var PersonBar = React.createClass({
-  render: function () {
-    return (
-      <DocumentTitle title="Person Bar">
-        <p>This is bar...</p>
-      </DocumentTitle>
-    );
-  }
-});
-
-var routes = (
-  <Route name="main" handler={Main} path="/">
-    <NotFoundRoute handler={NotFound}/>
-    <Route name="login" handler={Login} path="/login"/>
-    <Route name="app" handler={App} path="/">
-      <DefaultRoute name="home" handler={Dashboard}/>
-      <Route name="people" handler={People} path="people">
-        <Route name="person" handler={Person} path=":personId">
-          <Route name="person/foo" handler={PersonFoo} path="foo"/>
-          <Route name="person/bar" handler={PersonBar} path="bar"/>
-        </Route>
-      </Route>
-    </Route>
-  </Route>
-);
+var routes = require('./routes.jsx');
 
 module.exports = {
-  init: function (node) {
+  init: function (el) {
     return Router.run(routes, Router.HistoryLocation, function (Handler, state) {
       var params = state.params;
-      React.render(<Handler params={params} />, node);
+      React.render(<Handler params={params} />, el);
     });
   }
 };
---
+++ src/app/routes.jsx
@@ -0,0 +1,60 @@
+var React = require('react');
+var Router = require('react-router');
+var DocumentTitle = require('react-document-title');
+var App = require('./components/App.jsx');
+var People = require('./components/People.jsx');
+var Person = require('./components/Person.jsx');
+var Login = require('./components/Login.jsx');
+var Dashboard = require('./components/Dashboard.jsx');
+var RouteHandler = Router.RouteHandler;
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
+
+var NotFound = React.createClass({
+  render: function () {
+    return <p>Nope!</p>;
+  }
+});
+
+var Main = React.createClass({
+  render: function () {
+    return <RouteHandler {...this.props} />;
+  }
+});
+
+var PersonFoo = React.createClass({
+  render: function () {
+    return (
+      <DocumentTitle title="Person Foo">
+        <p>This is foo...</p>
+      </DocumentTitle>
+    );
+  }
+});
+
+var PersonBar = React.createClass({
+  render: function () {
+    return (
+      <DocumentTitle title="Person Bar">
+        <p>This is bar...</p>
+      </DocumentTitle>
+    );
+  }
+});
+
+module.exports = (
+  <Route name="main" handler={Main} path="/">
+    <NotFoundRoute handler={NotFound}/>
+    <Route name="login" handler={Login} path="/login"/>
+    <Route name="app" handler={App} path="/">
+      <DefaultRoute name="home" handler={Dashboard}/>
+      <Route name="people" handler={People} path="people">
+        <Route name="person" handler={Person} path=":personId">
+          <Route name="person/foo" handler={PersonFoo} path="foo"/>
+          <Route name="person/bar" handler={PersonBar} path="bar"/>
+        </Route>
+      </Route>
+    </Route>
+  </Route>
+);
