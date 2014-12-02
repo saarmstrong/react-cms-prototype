756d7c53e5f80f230115bc6ff7e5020d8a13f4ff
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2014-12-02 13:34:38 +0200

    adds not found route

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,14 +1,21 @@
 var React = require('react');
 var Router = require('react-router');
 var DocumentTitle = require('react-document-title');
-var Route = Router.Route;
-var DefaultRoute = Router.DefaultRoute;
-var RouteHandler = Router.RouteHandler;
 var App = require('./components/App.jsx');
 var People = require('./components/People.jsx');
 var Person = require('./components/Person.jsx');
 var Login = require('./components/Login.jsx');
 var Dashboard = require('./components/Dashboard.jsx');
+var RouteHandler = Router.RouteHandler;
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
+
+var NotFound = React.createClass({
+  render: function() {
+    return <p>Nope!</p>;
+  }
+});
 
 var Main = React.createClass({
   render: function() {
@@ -17,7 +24,8 @@ var Main = React.createClass({
 });
 
 var routes = (
-  <Route name="main" handler={Main}>
+  <Route name="main" handler={Main} path="/">
+    <NotFoundRoute handler={NotFound}/>
     <Route name="login" handler={Login} path="/login"/>
     <Route name="app" handler={App} path="/">
       <DefaultRoute name="home" handler={Dashboard}/>
