3828fd5745c7ae0d1ac2a737b863c9295cd4d622
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2014-12-02 17:12:21 +0200

    adds timer to dashboard

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -2,10 +2,28 @@ var React = require('react');
 var DocumentTitle = require('react-document-title');
 
 module.exports = React.createClass({
+  displayName: 'Dashboard',
+  getInitialState: function () {
+    return {
+      seconds: 0
+    };
+  },
+  componentDidMount: function () {
+    var self = this;
+    this.interval = setInterval(function () {
+      self.setState({seconds: self.state.seconds + 1});
+    }, 1000);
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.interval);
+  },
   render: function () {
     return (
       <DocumentTitle title="Dashboard">
-        <p>Dashboard</p>
+        <div>
+          <p>Dashboard</p>
+          <p>{this.state.seconds}s</p>
+        </div>
       </DocumentTitle>
     );
   }
