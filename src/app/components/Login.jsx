47e5485724dcab8f4f30fc4e77d3c7df1db57cb6
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2014-12-02 12:25:44 +0200

    adds login

---
+++ src/app/components/Login.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var DocumentTitle = require('react-document-title');
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <DocumentTitle title="Login">
+        <div className="container">
+          <br />
+          <br />
+          <p className="lead">Login</p>
+          <form>
+            <div className="form-group">
+              <input type="text" placeholder="Email Address" className="form-control" />
+            </div>
+            <div className="form-group">
+              <input type="password" placeholder="Password" className="form-control" />
+            </div>
+            <div className="form-group">
+              <input type="submit" className="btn btn-default" />
+            </div>
+          </form>
+        </div>
+      </DocumentTitle>
+    );
+  }
+});
--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,12 +1,13 @@
 var React = require('react');
 var Router = require('react-router');
+var DocumentTitle = require('react-document-title');
 var Route = Router.Route;
-var NotFoundRoute = Router.NotFoundRoute;
 var DefaultRoute = Router.DefaultRoute;
+var RouteHandler = Router.RouteHandler;
 var App = require('./components/App.jsx');
 var People = require('./components/People.jsx');
 var Person = require('./components/Person.jsx');
-var DocumentTitle = require('react-document-title');
+var Login = require('./components/Login.jsx');
 
 var Home = React.createClass({
   render: function() {
@@ -14,24 +15,20 @@ var Home = React.createClass({
   }
 });
 
-var Base = React.createClass({
+var Main = React.createClass({
   render: function() {
-    return null;
-  }
-});
-
-var NotFound = React.createClass({
-  render: function() {
-    return <p>Nope!</p>;
+    return <RouteHandler />;
   }
 });
 
 var routes = (
-  <Route name="app" handler={App} path="/">
-    <NotFoundRoute handler={NotFound}/>
-    <DefaultRoute name="home" handler={Home}/>
-    <Route name="people" handler={People} path="/people">
-      <Route name="person" handler={Person} path="/people/:personId"/>
+  <Route name="main" handler={Main}>
+    <Route name="login" handler={Login} path="/login"/>
+    <Route name="app" handler={App} path="/">
+      <DefaultRoute name="home" handler={Home}/>
+      <Route name="people" handler={People} path="/people">
+        <Route name="person" handler={Person} path="/people/:personId"/>
+      </Route>
     </Route>
   </Route>
 );
