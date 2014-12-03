bde1eff1776474bbb19fa192bab881c815252328
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2014-12-03 14:25:06 +0200

    load sir trevor using head.js

--- src/app/components/SirTrevor.jsx
+++ src/app/components/SirTrevor.jsx
@@ -1,14 +1,36 @@
 var React = require('react');
 var DocumentTitle = require('react-document-title');
 var PageTitle = require('./PageTitle.jsx');
+require('../../vendor/head.js');
 
 module.exports = React.createClass({
   getDefaultProps: function () {
     return {title: 'React + Sir Trevor'};
   },
+  getInitialState: function () {
+    return {
+      loading: true
+    };
+  },
+  load: function (callback) {
+    var self = this;
+    head.load('/vendor/sir-trevor.css');
+    head.load([
+      '/vendor/jquery.js',
+      '/vendor/lodash.js',
+      '/vendor/eventable.js',
+      '/vendor/sir-trevor.js'
+    ], function () {
+      self.setState({loading: false});
+      callback();
+    });
+  },
   componentDidMount: function () {
-    var $el = window.$(this.refs.editor.getDOMNode());
-    this.editor = new window.SirTrevor.Editor({ el: $el });
+    var self = this;
+    this.load(function () {
+      var $el = window.$(self.refs.editor.getDOMNode());
+      self.editor = new window.SirTrevor.Editor({ el: $el });
+    });
   },
   componentDidUnmount: function () {
     this.editor.destroy();
@@ -17,7 +39,7 @@ module.exports = React.createClass({
   submitHandler: function (e) {
     e.preventDefault();
     this.editor.onFormSubmit(true);
-    console.log(this.editor.dataStore);
+    console.log(this.editor.store.retrieve());
   },
   render: function () {
     return (
