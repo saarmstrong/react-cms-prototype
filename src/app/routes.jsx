acc6abcbf54f8ac19560d737577fd400a0a605bd
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2014-12-03 09:55:53 +0200

    you'll never guess what happened next...

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -13,24 +13,16 @@ var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
 var NotFoundRoute = Router.NotFoundRoute;
 
-var Main = React.createClass({
-  render: function () {
-    return <RouteHandler {...this.props} />;
-  }
-});
-
-module.exports = (
-  <Route name="main" handler={Main} path="/">
+module.exports = [
+  <Route name="login" handler={Login} path="/login"/>,
+  <Route name="app" handler={App} path="/">
     <NotFoundRoute handler={NotFound}/>
-    <Route name="login" handler={Login} path="/login"/>
-    <Route name="app" handler={App} path="/">
-      <DefaultRoute name="home" handler={Dashboard}/>
-      <Route name="people" handler={People} path="people">
-        <Route name="person" handler={Person} path=":personId">
-          <Route name="person/foo" handler={PersonFoo} path="foo"/>
-          <Route name="person/bar" handler={PersonBar} path="bar"/>
-        </Route>
+    <DefaultRoute name="home" handler={Dashboard}/>
+    <Route name="people" handler={People} path="people">
+      <Route name="person" handler={Person} path=":personId">
+        <Route name="person/foo" handler={PersonFoo} path="foo"/>
+        <Route name="person/bar" handler={PersonBar} path="bar"/>
       </Route>
     </Route>
   </Route>
-);
+];
